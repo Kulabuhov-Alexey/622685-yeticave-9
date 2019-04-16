d033005697d7654d0cd0172185988a01964ab1dc
kulabuha <kulabuha@inbox.ru>
2019-04-16 23:10:08 +0300

    Пишем первую функцию

--- index.php
+++ index.php
@@ -1,45 +1,58 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Alexey'; // укажите здесь ваше имя
-$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$items = [
-                    [
-                    "name" => "2014 Rossignol District Snowboard",
-                    "category" => $categories[0],
-                    "item_price" => "10999",
-                    "item_photo" => "img/lot-1.jpg"
-                    ],
-                    [
-                    "name" => "DC Ply Mens 2016/2017 Snowboard",
-                    "category" => $categories[0],
-                    "item_price" => "159999",
-                    "item_photo" => "img/lot-2.jpg"
-                    ],
-                    [
-                    "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
-                    "category" => $categories[1],
-                    "item_price" => "8000",
-                    "item_photo" => "img/lot-3.jpg"
-                    ],
-                    [
-                    "name" => "Ботинки для сноуборда DC Mutiny Charocal",
-                    "category" => $categories[2],
-                    "item_price" => "10999",
-                    "item_photo" => "img/lot-4.jpg"
-                    ],
-                    [
-                    "name" => "Куртка для сноуборда DC Mutiny Charocal",
-                    "category" => $categories[3],
-                    "item_price" => "7500",
-                    "item_photo" => "img/lot-5.jpg"
-                    ],
-                    [
-                    "name" => "Маска Oakley Canopy",
-                    "category" => $categories[5],
-                    "item_price" => "5400",
-                    "item_photo" => "img/lot-6.jpg"
-                    ],
+$categories = [
+    "boards" => "Доски и лыжи",
+    "attachment" => "Крепления",
+    "boots" => "Ботинки",
+    "clothing" => "Одежда",
+    "tools" => "Инструменты",
+    "other" => "Разное"
 ];
+$items = [
+[
+    "name" => "2014 Rossignol District Snowboard",
+    "category" => $categories[boards],
+    "item_price" => 10999,
+    "item_photo" => "img/lot-1.jpg"
+], [
+    "name" => "DC Ply Mens 2016/2017 Snowboard",
+    "category" => $categories[boards],
+    "item_price" => 159999,
+    "item_photo" => "img/lot-2.jpg"
+], [
+    "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
+    "category" => $categories[attachment],
+    "item_price" => 8000,
+    "item_photo" => "img/lot-3.jpg"
+], [
+   "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+   "category" => $categories[boots],
+   "item_price" => 10999,
+   "item_photo" => "img/lot-4.jpg"
+], [
+   "name" => "Куртка для сноуборда DC Mutiny Charocal",
+   "category" => $categories[clothing],
+   "item_price" => 7500,
+   "item_photo" => "img/lot-5.jpg"
+], [
+   "name" => "Маска Oakley Canopy",
+   "category" => $categories[other],
+   "item_price" => 5400,
+   "item_photo" => "img/lot-6.jpg"
+] 
+        ];
+
+/**
+ * @param integer $value
+ * функция для форматирования отображения цены
+ * @author KulabuhovAlexey
+ * @return string
+ */        
+function format_price ($value) {
+    //форматируем полученное число через встроенную ф-цию и добавляем пробел со знаком рубля
+    return number_format ($value , 0 , ' , ' , ' ')." &#8381;";    
+}        
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -117,7 +130,7 @@ $items = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount"><?= $item["item_price"]; ?></span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= format_price ($item["item_price"]);?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
