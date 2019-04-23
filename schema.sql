93744bc996ac1da20557132d2442c7fe4fad4964
kulabuha <kulabuha@inbox.ru>
2019-04-23 17:59:34 +0300

    Создание схемы БД личного проекта

--- schema.sql
+++ schema.sql
@@ -4,32 +4,26 @@ DEFAULT COLLATE utf8_general_ci;
 USE yeticave;
 
 CREATE TABLE categories (
-    name CHAR(30) PRIMARY KEY NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(30) NOT NULL,
     symbol_code CHAR(30) 
 );
-CREATE UNIQUE INDEX i_cat ON categories(name);
-
-INSERT INTO categories
-(name,symbol_code) VALUE ('Доски и лыжи', 'boards'),('Крепления', 'attachment'),('Ботинки', 'boots'),
-('Одежда', 'clothing'),('Инструменты', 'tools'),('Разное', 'other');
+CREATE INDEX i_cat ON categories(id);
+CREATE INDEX i_cat ON categories(name);
 
 CREATE TABLE stuff (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     name CHAR(30),
-    description VARCHAR(1000),
+    description TEXT,
     photo_url TEXT,
     start_price INT,
     dt_end TIMESTAMP,
     step_call INT,
     author_id CHAR,
     winner CHAR,
-    category CHAR,
-    FOREIGN KEY (category) REFERENCES categories(name),
-    FOREIGN KEY (author_id) REFERENCES users(email),
-    FOREIGN KEY (winner) REFERENCES users(email)      
+    category CHAR     
 ); 
-
 CREATE INDEX i_st_n ON stuff(name);
 CREATE INDEX i_st_d ON stuff(description(1000));
 CREATE INDEX i_st_i ON stuff(id);
@@ -37,11 +31,9 @@ CREATE INDEX i_st_i ON stuff(id);
 CREATE TABLE bet (
     id INT AUTO_INCREMENT PRIMARY KEY,
     dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ums INT,
+    total INT,
     author_id CHAR,
-    lot_id INT,
-    FOREIGN KEY (author_id) REFERENCES users(email),
-    FOREIGN KEY (lot_id) REFERENCES stuff(id) 
+    lot_id INT
 );
 CREATE INDEX i_bet ON bet(id);
 
@@ -53,8 +45,6 @@ CREATE TABLE users (
     avatar_url TEXT,
     contact TEXT,
     lot_id INT,
-    bet_id INT,
-    FOREIGN KEY (lot_id) REFERENCES stuff(id),
-    FOREIGN KEY (bet_id) REFERENCES bet(id)
+    bet_id INT
 );
 CREATE INDEX i_email ON users(email);
