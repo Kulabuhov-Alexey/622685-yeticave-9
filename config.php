b6faba3b890c96f8949a647c7661948e50d49a59
kulabuha <kulabuha@inbox.ru>
2019-05-01 11:05:40 +0300

    Чтение и показ записей из MySQL

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
+<?php 
 date_default_timezone_set("Europe/Moscow");
+
+$host = 'localhost'; // адрес сервера 
+$database = 'yeticave'; // имя базы данных
+$user = 'root'; // имя пользователя
+$password = ''; // пароль
+
+$finish_sell_time = 'tomorrow midnight';// время до которого считается конец распродажи
